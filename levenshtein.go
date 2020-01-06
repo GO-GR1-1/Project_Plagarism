d00426544cf756c51b0e16d4dcbf4a63810f1440
HugoCo <hug1910@live.fr>
2020-01-06 18:47:09 +0100

    Add files via upload

---
+++ levenshtein.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/agnivade/levenshtein"
+)
+
+func main() {
+	s1 := "Il fait beau"
+	s2 := "Il a fait beau"
+	distance := levenshtein.ComputeDistance(s1, s2)
+	fmt.Printf("The distance between %s and %s is %d.\n", s1, s2, distance)
+	// Output:
+	// The distance between kitten and sitting is 3.
+}
