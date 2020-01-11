b32b963474bebe86b3ca0336b05fcb67d8ba80f2
qbordignon <quentin.bordignon@insa-lyon.fr>
2020-01-11 17:00:31 +0100

    Update test_bis.go

--- test_bis.go
+++ test_bis.go
@@ -10,6 +10,12 @@ import (
     //"github.com/agnivade/levenshtein"
 )
 
+//Type phrase. Contient un string qui correspond au contenu de la phrase et un booléen qui indique si la phrase est une phrase plagiée ou non
+type sentence struct{
+	text string
+	boolean bool
+}
+
 var wg sync.WaitGroup
 var wg2 sync.WaitGroup
 
@@ -33,8 +39,8 @@ func openText(text string) string{
 	return string
 }
 
-//Fonction qui prend en paramètre un fichier .txt, l'ouvre et nous renvoie un tableau de String contenant les phrases du texte 
-func splitText(text string) []string{
+//Fonction qui prend en paramètre un fichier .txt, l'ouvre et nous renvoie un tableau de sentence
+func splitText(text string) []sentence{
 
 	//Ouvre le texte et stocke le String lui correspondant
 	string := openText(text)
@@ -42,9 +48,18 @@ func splitText(text string) []string{
 	//On prend la chaine de caractere qu'on split en phrase avec le point comme separateur
 	string_tab := strings.Split(string, ".")
 
+	var sentence_tab []sentence
+	var sentence_in_text sentence
+
+	for s:=0; s<len(string_tab); s++{
+		sentence_in_text = sentence{text: string_tab[s], boolean : false}
+		sentence_tab = append(sentence_tab, sentence_in_text)
+		//fmt.Println(string(sentence_tab[s].boolean))
+	}
+
   	fmt.Println("DEBUG Fin du split")
   	
-  	return string_tab
+  	return sentence_tab
 }
 
 //Fonction qui renvoie un tableau contenant le nom de tous les fichiers .txt du répertoire courant
@@ -67,29 +82,30 @@ func textFilesInDirectory() []string {
 
 //Fonction qui prend en paramètre la phrase à comparer, le texte dans lequel il faut regarder et son nom et 
 //qui affiche dans la console si cette phrase appartient au texte
-func stringInText(s1 string, s2 string, s2_name string){
+func stringInText(s1 *sentence, s2 string, s2_name string){
 	
 	//On teste si la phrase est dans le texte
-	string_in_text := strings.Contains(s2, s1)
+	string_in_text := strings.Contains(s2, s1.text)
 
 	//Si oui, on affiche
 	if string_in_text {
-		fmt.Println("From " + s2_name + " : " + s1)
+		fmt.Println("From " + s2_name + " : " + s1.text)
+		s1.boolean = true
 	}
 
 	wg2.Done()
 }
 
 //Fonction qui prend en paramètre le texte de la base de données et le texte reçu pour les comparer
-func compareText(database_text_name string, received_text []string){
+func compareText(database_text_name string, received_text *[]sentence){
 	
 	//On ouvre le fichier de la base de données
 	database_text := openText(database_text_name)
 
 	//Pour chaque phrase du texte reçu on regarde si elle est dans le texte de la base de données
-	for s:=0; s<len(received_text); s++ {
+	for s:=0; s<len(*received_text); s++ {
 		wg2.Add(1)
-		go stringInText(received_text[s], database_text, database_text_name)
+		go stringInText(&(*received_text)[s], database_text, database_text_name)
 	}
 
 	wg2.Wait()
@@ -102,12 +118,28 @@ func main() {
 	received_text := splitText("prez.txt")
 	text_files := textFilesInDirectory()
 	for t:=0; t<len(text_files); t++ {
-		wg.Add(1)
-		go compareText(text_files[t], received_text)
+		if text_files[t] != "prez.txt"{
+			wg.Add(1)
+			go compareText(text_files[t], &received_text)
+		}
 	}
 
 	fmt.Println("DEBUG WAITING")
 	wg.Wait()
 
+	var counter int
+	var total int
+
+	for k:=0; k<len(received_text); k++{
+		if received_text[k].boolean == true {
+			counter = counter + len(received_text[k].text)
+		}
+		total = total + len(received_text[k].text)
+	}
+
+	ratio := (float32(counter)/float32(total))*100
+
+	fmt.Printf("Plagiarism score : %f%", ratio)
+	fmt.Println()
 	fmt.Println("DEBUG END")
 }
