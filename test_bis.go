42ffc89a01c097fbe6db6a8981e63101d840ab09
qbordignon <quentin.bordignon@insa-lyon.fr>
2020-01-11 16:59:08 +0100

    Update test_bis.go

--- test_bis.go
+++ test_bis.go
@@ -10,9 +10,10 @@ import (
     //"github.com/agnivade/levenshtein"
 )
 
-var waitingGroup sync.WaitGroup
-var waitingGroup2 sync.WaitGroup
+var wg sync.WaitGroup
+var wg2 sync.WaitGroup
 
+//Fonction qui prend en paramètre un fichier .txt, l'ouvre et nous renvoie ce texte sous forme de String 
 func openText(text string) string{
 	//Ouverture du fichier
     file, err := os.Open(text)
@@ -32,23 +33,21 @@ func openText(text string) string{
 	return string
 }
 
+//Fonction qui prend en paramètre un fichier .txt, l'ouvre et nous renvoie un tableau de String contenant les phrases du texte 
 func splitText(text string) []string{
 
+	//Ouvre le texte et stocke le String lui correspondant
 	string := openText(text)
 
 	//On prend la chaine de caractere qu'on split en phrase avec le point comme separateur
 	string_tab := strings.Split(string, ".")
-
-	//Affichage des phrases
-	//for i := 0; i<len(string_tab); i++ {
-  	//	fmt.Println(string_tab[i])
-  	//}
 
   	fmt.Println("DEBUG Fin du split")
   	
   	return string_tab
 }
 
+//Fonction qui renvoie un tableau contenant le nom de tous les fichiers .txt du répertoire courant
 func textFilesInDirectory() []string {
 	files, err := ioutil.ReadDir(".")
     if err != nil {
@@ -66,43 +65,49 @@ func textFilesInDirectory() []string {
     return textFiles
 }
 
-func stringInText(s1 string, s2 string, s2_name string) {
-	bool := strings.Contains(s2, s1)
+//Fonction qui prend en paramètre la phrase à comparer, le texte dans lequel il faut regarder et son nom et 
+//qui affiche dans la console si cette phrase appartient au texte
+func stringInText(s1 string, s2 string, s2_name string){
+	
+	//On teste si la phrase est dans le texte
+	string_in_text := strings.Contains(s2, s1)
 
-	if bool == true {
+	//Si oui, on affiche
+	if string_in_text {
 		fmt.Println("From " + s2_name + " : " + s1)
 	}
-	
-	waitingGroup2.Done()
-	//distance := levenshtein.ComputeDistance(s1, s2)
-	//fmt.Printf("The distance between %s and %s is %d.\n", s1, s2, distance)
-	// Output:
-	// The distance between kitten and sitting is 3.
+
+	wg2.Done()
 }
 
-func compareText(text string, textToCompare []string){
-	bd := openText(text)
-	for s:=0; s<len(textToCompare); s++ {
-		waitingGroup2.Add(1)
-		go stringInText(textToCompare[s], bd, text)
+//Fonction qui prend en paramètre le texte de la base de données et le texte reçu pour les comparer
+func compareText(database_text_name string, received_text []string){
+	
+	//On ouvre le fichier de la base de données
+	database_text := openText(database_text_name)
+
+	//Pour chaque phrase du texte reçu on regarde si elle est dans le texte de la base de données
+	for s:=0; s<len(received_text); s++ {
+		wg2.Add(1)
+		go stringInText(received_text[s], database_text, database_text_name)
 	}
 
-	waitingGroup2.Wait()
+	wg2.Wait()
 
-	waitingGroup.Done()
+	wg.Done()
 }
 
 func main() {
 	fmt.Println("DEBUG START")
-	receivedText := splitText("prez.txt")
-	textFiles := textFilesInDirectory()
-	for t:=0; t<len(textFiles); t++ {
-		waitingGroup.Add(1)
-		go compareText(textFiles[t], receivedText)
+	received_text := splitText("prez.txt")
+	text_files := textFilesInDirectory()
+	for t:=0; t<len(text_files); t++ {
+		wg.Add(1)
+		go compareText(text_files[t], received_text)
 	}
 
 	fmt.Println("DEBUG WAITING")
-	waitingGroup.Wait()
+	wg.Wait()
 
 	fmt.Println("DEBUG END")
 }
